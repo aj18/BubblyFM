30cec2944853fce4370a279b3fb33992c048d0f6
Arun <arunyadav13@gmail.com>
2015-09-05 00:03:44 +0530

    Pcard

---
+++ flowly/PhotoDynamic.js
@@ -0,0 +1,20 @@
+var React = require("react");
+
+var PhotoDynamic = React.createClass({
+    render : function() {
+      
+        var imageContainerStyle = {width: '100%', height: '400', overflow: 'hidden' };
+        
+        var imgStyle = { width: '100%' };
+        var s4={opacity: '1',float: 'left',height:this.props.pictureStyle.height,overflow: 'hidden',width:'100%', display: 'block', 'background-image': 'url('+this.props.url+')', 'background-size': 'cover','background-position':'25% 0%', 'background-repeat': 'no-repeat' };
+    
+        return (<div style={s4}>
+                
+            </div>);
+
+}
+
+});
+
+
+module.exports = PhotoDynamic;
